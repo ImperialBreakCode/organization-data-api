1e7d5f1ac216a4b3ae7b77704b45cfa6dfe2e9ba
ImperialBreakCode <eeeda522@gmail.com>
2023-12-24 23:07:59 +0200

    adding repos to db context interface

--- OrganizationDataAPI/DataAccess/OrganizationData.Data.Abstractions/DbContext/IOrganizationDbContext.cs
+++ OrganizationDataAPI/DataAccess/OrganizationData.Data.Abstractions/DbContext/IOrganizationDbContext.cs
@@ -1,7 +1,14 @@
-﻿namespace OrganizationData.Data.Abstractions.DbContext
+﻿using OrganizationData.Data.Abstractions.Repository;
+using OrganizationData.Data.Entities;
+
+namespace OrganizationData.Data.Abstractions.DbContext
 {
     public interface IOrganizationDbContext
     {
+        IOrganizationRepository Organization { get; }
+        IRepository<Country> Country { get; }
+        IRepository<Industry> Industry { get; }
+
         void SaveChanges();
     }
 }
